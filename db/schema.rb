29440eed12690a6238f22e1ff197f2407576707c
mauro-au <mauricio.a.developer@gmail.com>
2025-01-08 08:53:07 -0300

    Se agregan el poder comentar en cada foto

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_08_080402) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_08_111429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_08_080402) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "photo_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "photos", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -65,4 +75,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_08_080402) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "photos"
+  add_foreign_key "comments", "users"
 end
